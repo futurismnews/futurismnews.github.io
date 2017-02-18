caf5bff3d23fe68e9585838cc26a28bdaa1572fa
futurismnews <zwest135@gmail.com>
2017-02-18 17:36:17 -0600

    uploaded website for first time

---
+++ javascriptstuff.js
@@ -0,0 +1,6 @@
+<script>
+var VRAITIS-hl1 = "'VRAITIS', VR induced epidemic spreads across globe";
+var VRAITIS-hl2="UN calls public health hearing"
+var VRAITIS-p1 ="
+document.getElementById("test").innerHTML = "teststst";
+</script>
